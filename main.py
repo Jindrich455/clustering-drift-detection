a9014d7dc975091a91f5216e0b07ee18e440e9aa
Jindrich Pohl <j.pohl@student.tudelft.nl>
2022-11-30 19:08:38 +0100

    Fix one-hot encoding with categorical data

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 # This is a sample Python script.
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MinMaxScaler, OneHotEncoder
 
 import accepting
 import preprocessing
 import ucdd
 
 if __name__ == '__main__':
-    df_x, df_y = accepting.get_clean_df('datasets/sea_1_abrupt_drift_0_noise_balanced.arff')
+    df_x, df_y = accepting.get_clean_df('datasets/agraw1_1_abrupt_drift_0_noise_balanced.arff')
     X_ref_batches, y_ref_batches, X_test_batches, y_test_batches = preprocessing.prepare_data_and_get_batches(
         df_x, df_y, test_fraction=0.7, num_ref_batches=3, num_test_batches=7,
-        scaling=True, scaler=MinMaxScaler(), use_categorical=False, encoding=False, encoder=None
+        scaling=True, scaler=MinMaxScaler(), use_categorical=False, encoding=False, encoder=OneHotEncoder(sparse=False)
     )
 
     drift_occurrences = ucdd.drift_occurrences_list(X_ref_batches, X_test_batches, random_state=0)
--- preprocessing.py
+++ preprocessing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 import sklearn.model_selection
 
 
@@ -8,19 +9,38 @@ def divide_numeric_categorical(df_x):
     return df_x_numeric, df_x_categorical
 
 
-def transform_df_with(df, transformer):
+def scale_df_with(df, transformer):
     df[df.columns] = transformer.fit_transform(df[df.columns])
+    return df
+
+
+def encode_df_with(df, transformer):
+    original_cols = df.columns
+
+    col = transformer.fit_transform(df[df.columns])
+    df_col = pd.DataFrame(col)
+    # make sure all column names are strings
+    df_col.columns = [str(cname) for cname in df_col.columns]
+    print('df_col')
+    print(df_col)
+    df = df.join(df_col)
+    print('joined')
+    print(df)
+    df = df.drop(columns=original_cols)
+    print('dropped')
+    print(df)
+
     return df
 
 
 def prepare_data(df_x, scaling, scaler, use_categorical, encoding=False, encoder=None):
     df_x_num, df_x_cat = divide_numeric_categorical(df_x)
     if scaling:
-        transform_df_with(df_x_num, scaler)
+        df_x_num = scale_df_with(df_x_num, scaler)
 
     if use_categorical:
         if encoding:
-            transform_df_with(df_x_cat, encoder)
+            df_x_cat = encode_df_with(df_x_cat, encoder)
         df_x = df_x_num.join(df_x_cat)
     else:
         df_x = df_x_num
@@ -29,8 +49,8 @@ def prepare_data(df_x, scaling, scaler, use_categorical, encoding=False, encoder
 
 
 def prepare_data_and_get_batches(df_x, df_y, test_fraction, num_ref_batches, num_test_batches,
-                                 scaling, scaler, use_categorical, encoding=False, encoder=None):
-    df_x = prepare_data(df_x, scaling, scaler, use_categorical, encoding=False, encoder=None)
+                                 scaling, scaler, use_categorical=False, encoding=False, encoder=None):
+    df_x = prepare_data(df_x, scaling, scaler, use_categorical, encoding, encoder)
 
     df_X_ref, df_X_test, df_y_ref, df_y_test = sklearn.model_selection.train_test_split(
         df_x, df_y, test_size=test_fraction, shuffle=False)
