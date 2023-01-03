8a4d6b8bbda439a7a21f3971f7c3148ba886893c
Jindrich Pohl <j.pohl@student.tudelft.nl>
2023-01-03 12:44:16 +0100

    Clean the folder and file structure

--- main.py
+++ main.py
@@ -118,7 +118,7 @@ if __name__ == '__main__':
     # experiments.mssw_write_to_file_mixed_exclude()
     # experiments.mssw_write_to_file_multiple_clustering_possibilities()
     # experiments.mssw_write_to_file_multiple_encoding_strategies()
-    experiments.mssw_write_to_file_progressively_changing_clustering_parameters()
+    # experiments.mssw_write_to_file_progressively_changing_clustering_parameters()
 
     # messing_around.f2()
     # messing_around.f2(10)
