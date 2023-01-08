52156c02e056329c0d98736d2f2cd4fe7ec10ad1
Jindrich Pohl <j.pohl@student.tudelft.nl>
2023-01-08 14:58:46 +0100

    Obtain results for mssw

---
+++ mssw/present_results.py
@@ -0,0 +1,31 @@
+import csv
+
+import pandas as pd
+
+
+common_path = 'results_after_analysis/'
+rel_paths = [
+    'sea',
+    'agraw1_exclude',
+    'agraw1_onehot',
+    'agraw1_target',
+    'agraw2_exclude',
+    'agraw2_onehot',
+    'agraw2_target'
+]
+postfix = '.csv'
+
+all_results = pd.read_csv(common_path + rel_paths[0] + postfix)
+
+for rel_path in rel_paths[1:]:
+    res = pd.read_csv(common_path + rel_path + postfix)
+    all_results = all_results.append(res)
+
+
+print(all_results.to_string())
+all_results = all_results.drop(columns=['type_of_data', 'n_init', 'max_iter', 'tol'])
+all_results.to_csv(common_path + 'everything.csv')
+
+
+# all_agraw = res_dict['agraw1_exclude'].join(res_dict['agraw1_onehot'].join(res_dict['agraw1_target']))
+# print('all agraw', all_agraw)
