c9ebaf6792ec10e1d7b6404a8dffbff25d4211f4
Jindrich Pohl <j.pohl@student.tudelft.nl>
2023-01-09 23:18:00 +0100

    Make plots for a visual analysis of ucdd and mssw on synthetic data

--- ucdd_improved/present_results.py
+++ ucdd_improved/present_results.py
@@ -1,6 +1,7 @@
 import csv
 
 import pandas as pd
+from matplotlib import pyplot as plt
 
 
 common_path = 'results_after_analysis/'
@@ -42,6 +43,39 @@ agraw1_meaningful = all_results.loc[(all_results['dataset'] == 'agraw1')
 print('agraw1 meaningful')
 print(agraw1_meaningful.to_string())
 
+# colors = ["red", "green", "blue"]
+# styles = ["-", "--", "-."]
+# labels = ['encoding=exclude', 'encoding=onehot', 'encoding=target']
+# agraw1_meaningful_with_check = agraw1_meaningful[agraw1_meaningful['additional_check'] == 'yes']
+# for i in range(3):
+#     agraw1_meaningful_one_result_set = agraw1_meaningful_with_check.iloc[(i * 6):((i + 1) * 6), :]
+#     # line1, = plt.plot(agraw1_meaningful_one_result_set['width'], agraw1_meaningful_one_result_set['FPR_mean'],
+#     #                   color=colors[i], linestyle=styles[i], marker='o',
+#     #                   label=labels[i])
+#     line1, = plt.plot(agraw1_meaningful_one_result_set['width'], agraw1_meaningful_one_result_set['latency_mean'],
+#                       color=colors[i], linestyle=styles[i], marker='o',
+#                       label=labels[i])
+# plt.legend()
+# plt.ylim((0, 1))
+# plt.show()
+
+
+# colors = ["red", "green", "blue"]
+# styles = ["-", "--", "-."]
+# labels = ['encoding=exclude', 'encoding=onehot', 'encoding=target']
+# agraw1_meaningful_no_check = agraw1_meaningful[agraw1_meaningful['additional_check'] == 'no']
+# for i in range(3):
+#     agraw1_meaningful_one_result_set = agraw1_meaningful_no_check.iloc[(i * 6):((i + 1) * 6), :]
+#     # line1, = plt.plot(agraw1_meaningful_one_result_set['width'], agraw1_meaningful_one_result_set['FPR_mean'],
+#     #                   color=colors[i], linestyle=styles[i], marker='o',
+#     #                   label=labels[i])
+#     line1, = plt.plot(agraw1_meaningful_one_result_set['width'], agraw1_meaningful_one_result_set['latency_mean'],
+#                       color=colors[i], linestyle=styles[i], marker='o',
+#                       label=labels[i])
+# plt.legend()
+# plt.ylim((0, 1))
+# plt.show()
+
 
 # ag1 excl submaj yes
 
@@ -55,8 +89,24 @@ agraw2_meaningful = all_results.loc[(all_results['dataset'] == 'agraw2')
                                     & (all_results['train_batch_strategy'] == 'submajority')
                                     & (all_results['encoding'] == 'onehot')]
 
-print('agraw2 meaningful')
-print(agraw2_meaningful.to_string())
+# print('agraw2 meaningful')
+# print(agraw2_meaningful.to_string())
+#
+colors = ["red", "green"]
+styles = ["-", "--"]
+labels = ['no_additional_check', 'with_additional_check']
+for i in range(2):
+    agraw2_meaningful_one_result_set = agraw2_meaningful.iloc[(i * 6):((i + 1) * 6), :]
+    # line1, = plt.plot(agraw2_meaningful_one_result_set['width'], agraw2_meaningful_one_result_set['FPR_mean'],
+    #                   color=colors[i], linestyle=styles[i], marker='o',
+    #                   label=labels[i])
+    line1, = plt.plot(agraw2_meaningful_one_result_set['width'], agraw2_meaningful_one_result_set['latency_mean'],
+                      color=colors[i], linestyle=styles[i], marker='o',
+                      label=labels[i])
+plt.legend()
+plt.ylim((0, 1))
+plt.show()
+
 
 # ag2 1hot submaj no
 # ag2 1hot submaj yes
@@ -66,6 +116,21 @@ sea_meaningful = all_results.loc[(all_results['dataset'] == 'sea')
 
 print('sea meaningful')
 print(sea_meaningful.to_string())
+
+# colors = ["red", "green", "blue"]
+# styles = ["-", "--", "-."]
+# labels = ['train_batch_strategy=all', 'train_batch_strategy=majority', 'train_batch_strategy=submajority']
+# for i in range(3):
+#     sea_meaningful_one_result_set = sea_meaningful.iloc[(i * 6):((i + 1) * 6), :]
+#     # line1, = plt.plot(sea_meaningful_one_result_set['width'], sea_meaningful_one_result_set['FPR_mean'],
+#     #                   color=colors[i], linestyle=styles[i], marker='o',
+#     #                   label=labels[i])
+#     line1, = plt.plot(sea_meaningful_one_result_set['width'], sea_meaningful_one_result_set['latency_mean'],
+#                       color=colors[i], linestyle=styles[i], marker='o',
+#                       label=labels[i])
+# plt.legend()
+# plt.ylim((0, 1))
+# plt.show()
 
 # sea excl all yes
 # sea excl maj yes
