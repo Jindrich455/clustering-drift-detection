dd5a005785319b60f816e730f2c0888afc4bdddd
Jindrich Pohl <j.pohl@student.tudelft.nl>
2023-01-17 14:30:04 +0100

    Adapt the results to the newly introduced min_ref_batches_drift

--- ucdd_improved/present_results_jupyter.py
+++ ucdd_improved/present_results_jupyter.py
@@ -7,7 +7,7 @@ print(os.listdir('results_after_analysis_jupyter'))
 path_to_results = 'results_after_analysis_jupyter'
 for filename in os.listdir(path_to_results):
     df = pd.read_csv(path_to_results + '/' + filename)
-    name = '_'.join([df['dataset'].iloc[0], df['encoding'].iloc[0], df['train_batch_strategy'].iloc[0],
+    name = '_'.join([df['dataset'].iloc[0], df['encoding'].iloc[0], 'minref' + str(df['min_ref_batches_drift'].iloc[0]),
            ('yescheck' if df['additional_check'].iloc[0] == 'yes' else 'nocheck')])
     print(name)
 
